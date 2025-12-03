002b4f30b96e95852101159ec71c5bfdea513c91
Britney Hawthorne <britneyhawthorne@Britneys-MacBook-Air.local>
2025-12-03 15:42:08 -0500

    Exercise 3

--- world.php
+++ world.php
@@ -1,17 +1,28 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+
+// Get the GET variable (if it exists)
+$country = isset($_GET['country']) ? $_GET['country'] : "";
+
+// If a country was typed in, search for it
+if ($country !== "") {
+    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    $stmt->execute(['country' => "%$country%"]);
+} else {
+    // If no country was typed in, return all
+    $stmt = $conn->query("SELECT * FROM countries");
+}
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
+
